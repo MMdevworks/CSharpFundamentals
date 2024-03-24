aed9815b428be1c7bf4ba59b5c21d5820833fabb
Marie Marcos <mmdevq@gmail.com>
2024-03-24 12:51:03 -0700

    init

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("What's your name?");
+
+// ? null conditional operator. Allows null value
+string? str = Console.ReadLine();
+
+Console.WriteLine("Welcome to the world " + str + "!");
