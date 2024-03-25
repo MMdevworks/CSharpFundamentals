f30cff98f4e86e66693eb76b48cc483f0594cab0
Marie Marcos <mmdevq@gmail.com>
2024-03-24 20:21:48 -0700

    exceptions

---
+++ ProgramFlow/Exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿int x = 1030;
+int y = 0;
+int result;
+
+// TODO: try-catch expressions make error checking easier
+try {
+    if (x > 1000) {
+        throw new ArgumentOutOfRangeException("x", "x has to be 1000 or less");
+    }
+    result = x / y;
+    Console.WriteLine("The result is: {0}", result);
+}
+catch (DivideByZeroException e) {
+    Console.WriteLine("Can't divide by 0!");
+    Console.WriteLine(e.Message);
+}
+catch (ArgumentOutOfRangeException e) {
+    Console.WriteLine("1000 is the limit!");
+    Console.WriteLine(e.Message);
+}
+finally {
+    Console.WriteLine("finally code always runs");
+}
