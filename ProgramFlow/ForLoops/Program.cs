8f73fc0dcdb19e05f09058abbd9e3b9f3021380d
Marie Marcos <mmdevq@gmail.com>
2024-03-24 19:47:46 -0700

    loops

--- ProgramFlow/ForLoops/Program.cs
+++ ProgramFlow/ForLoops/Program.cs
@@ -3,12 +3,20 @@ int[] nums = new int[] {3, 14, 15, 92, 6};
 string str = "The quick brown fox jumps over the lazy dog";
 
 // TODO: the basic for loop
-Console.WriteLine("The basic for loop:");
-
-Console.WriteLine();
+for (int i = 0; i < myVal; i++) {
+    Console.WriteLine("i is currently {0}", i);
+}
 
 // TODO: the foreach-in loop can be used to iterate over sequences
-// Console.WriteLine("The foreach loop:");
-
+foreach (int i in nums) {
+    Console.WriteLine("i is currently {0}", i);
+}
 
 // TODO: count the number of o's in the string
+var count = 0;
+foreach (char c in str) {
+    if (c == 'o') {
+        count++;
+    }
+}
+Console.WriteLine("Counted {0} o characters", count);
