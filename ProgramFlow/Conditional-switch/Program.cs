5caf2265cbaee153a9e827e0f4215bdc198fd04a
Marie Marcos <mmdevq@gmail.com>
2024-03-24 19:26:15 -0700

    conditionals

---
+++ ProgramFlow/Conditional-switch/Program.cs
@@ -0,0 +1,20 @@
+﻿int theVal = 50;
+
+// TODO: The switch statement, used when if elses become too numerous 
+// conditions followed by break if condition is met
+switch (theVal) {  //the switch can use any non null expression: int, str, char, bool ect
+case 50:
+    Console.WriteLine("theVal is 50");
+    break;
+case 51:
+    Console.WriteLine("theVal is 51");
+    break;
+case 52: //cases can also be grouped as such
+case 53:
+case 54:
+    Console.WriteLine("theVal is between 52 and 54");
+    break;
+default: //default is like an else catchall
+    Console.WriteLine("theVal is something else");
+    break;
+}
---
+++ ProgramFlow/ForLoops/Program.cs
@@ -0,0 +1,14 @@
+﻿int myVal = 15;
+int[] nums = new int[] {3, 14, 15, 92, 6};
+string str = "The quick brown fox jumps over the lazy dog";
+
+// TODO: the basic for loop
+Console.WriteLine("The basic for loop:");
+
+Console.WriteLine();
+
+// TODO: the foreach-in loop can be used to iterate over sequences
+// Console.WriteLine("The foreach loop:");
+
+
+// TODO: count the number of o's in the string
