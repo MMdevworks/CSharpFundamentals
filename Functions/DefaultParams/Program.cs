1f3d90341ca909a1f7138a149b30bacdaad45271
Marie Marcos <mmdevq@gmail.com>
2024-03-25 17:21:19 -0700

    default params

---
+++ Functions/DefaultParams/Program.cs
@@ -0,0 +1,12 @@
+﻿// TODO: Functions can provide default values for their parameters
+void PrintWithPrefix(string str, string prefix="")
+{
+    Console.WriteLine($"{prefix} {str}");
+}
+
+// TODO: Test the default parameters
+PrintWithPrefix("Hello There!");
+PrintWithPrefix("Hello There!", ">: ");
+
+// TODO: Call with named params, in any order
+PrintWithPrefix(prefix: "% ", str: "Hello There!");
