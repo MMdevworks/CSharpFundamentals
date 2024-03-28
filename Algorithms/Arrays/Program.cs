a12f04fa3314167eaa571900c9bd64aebc3de9d1
Marie Marcos <mmdevq@gmail.com>
2024-03-27 20:54:59 -0700

    linkedlist

---
+++ Algorithms/Arrays/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+
+//  ------------ Linear Search
+static Boolean LinearSearch(int[] input, int n) {
+    foreach(int current in input) {
+        if (n == current) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int[] arr = {1, 2, 3, 4, 5, 6};
+// LinearSearch(arr, 4);
+// Console.WriteLine(LinearSearch(arr, 4));
+// Console.WriteLine(LinearSearch(arr, 8));
+
+int item = Array.Find(arr, element => element == 3);
+// Console.WriteLine(item);
+
+int[] items = Array.FindAll(arr, element => element >= 5);
+// Array.ForEach(items, Console.WriteLine);
+
+//  ------------ Binary Search
+static Boolean BinarySearch(int[] inputArray, int item) {
+    int min = 0;
+    int max = inputArray.Length -1;
+
+    while (min <= max) {
+        int mid = (min + max) / 2;
+        if(item == inputArray[mid]) {
+            return true;
+        } else if (item < inputArray[mid]) {
+            max = mid - 1;
+        } else {
+            min = mid + 1;
+        }
+    }
+    return false;
+}
+
+// int[] array = {1, 2, 3, 4, 5, 6};
+// Console.WriteLine(BinarySearch(array, 5));
+
+//  ------------ Aggregating Valid Contents from two arrays
+static int[] FindEvenNums(int[] arr1, int[] arr2) {
+    ArrayList result = new ArrayList();
+
+    foreach(int num in arr1) {
+        if (num % 2 == 0) {
+            result.Add(num);
+        }
+    }
+    foreach(int num in arr2) {
+        if (num % 2 == 0) {
+            result.Add(num);
+        }
+    }
+    return (int[])result.ToArray(typeof(int));
+}
+
+// int[] arr1 = {-8, 2, 3, -9, 11, -20};
+// int[] arr2 = {0, -2, -9, -39, 39, 10, 7};
+
+// int[] evenArr = FindEvenNums(arr1, arr2);
+// Array.ForEach(evenArr, Console.WriteLine);
+
+//  ------------ Reverse Array
+static int[] Reverse(int[] input) {
+    int[] reversed = new int[input.Length];
+
+    for (int i = 0; i < reversed.Length; i++) {
+        reversed[i] = input[input.Length - i - 1];
+    }
+    return reversed;
+}
+
+static void ReverseInPlace(int[] input) {
+    for (int i = 0; i < input.Length / 2; i++) {
+        int temp = input[i];
+        input[i] = input[input.Length - i - 1];
+        input[input.Length -i - 1] = temp;
+    }
+}
+
+int[] array1 = {1,2,3,4,5,6};
+// Array.ForEach(Reverse(array1), Console.WriteLine);
+ReverseInPlace(array1);
+Array.ForEach(array1, Console.WriteLine);
---
+++ Algorithms/String/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.Linq;
+using System.Text;
+
+
+// ------------- Validate Strings
+static Boolean isUpperCase(string s) {
+    return s.All(char.IsUpper);
+}
+
+static Boolean isLowerCase(string s) {
+    return s.All(char.IsLower);
+}
+
+static Boolean isPasswordComplex(string s) {
+    return s.Any(char.IsUpper) && s.Any(char.IsLower)
+    && s.Any(char.IsDigit);
+}
+
+// Console.WriteLine(isUpperCase("hello"));
+// Console.WriteLine(isLowerCase("hello"));
+
+// Console.WriteLine(isPasswordComplex("Hell0"));
+// Console.WriteLine(isPasswordComplex("HelLo"));
+// Console.WriteLine(isPasswordComplex("hello"));
+// Console.WriteLine(isPasswordComplex(""));
+
+// ------------- Normalize Strings, one line
+static string NormalizeString(String input) {
+    return input.ToLower().Trim().Replace(",","");
+}
+
+// Console.WriteLine(NormalizeString(" Make lowercase, TRIM End spaces and remove ,     "));
+
+// ------------- Parse and Search Strings
+static void ParseContents(string s) {
+    Console.WriteLine("Option 1");
+
+    foreach(char c in s) {
+        Console.WriteLine(c);
+    }
+
+    Console.WriteLine("Option 2");
+
+    for(int i = 0; i < s.Length; i++) {
+        char c = s[i];
+        Console.WriteLine(c);
+    }
+}
+
+static Boolean IsAtEvenIndex(string s, char item){
+    if (string.IsNullOrEmpty(s)){
+        return false;
+    }
+
+    for(int i = 0; i < s.Length / 2 + 1; i=i+2) {
+        if(s[i] == item) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Console.WriteLine("Hello World".ToLower().Contains("ll"));
+// ParseContents("Hello World");
+// String input = "HeLLo";
+// Console.WriteLine(IsAtEvenIndex(input, 'H'));
+// Console.WriteLine(IsAtEvenIndex(input, 'T'));
+// Console.WriteLine(IsAtEvenIndex(input, 'L'));
+// Console.WriteLine(IsAtEvenIndex(input, 'l'));
+// Console.WriteLine(IsAtEvenIndex("", 'H'));
+// Console.WriteLine(IsAtEvenIndex(null, 'H'));
+
+// ------------- Create algorithm driven strings
+static string Reverse(string input) {
+    if(string.IsNullOrEmpty(input)) {
+        return input;
+    }
+    StringBuilder reversed = new StringBuilder(input.Length);
+
+    for (int i = input.Length - 1; i >= 0; i--) {
+        reversed.Append(input[i]);
+    }
+    return reversed.ToString();
+}
+
+// Console.WriteLine(Reverse("Hello World"));
+// Console.WriteLine(Reverse(""));
+// Console.WriteLine(Reverse(null));
+
+// Option 2, using array and array method .Reverse()
+static string Reverse2(string input) {
+    if(string.IsNullOrEmpty(input)) {
+        return input;
+    }
+
+    char[] arr = input.ToCharArray();
+    Array.Reverse(arr);
+    return new string(arr);
+}
+
+// Console.WriteLine(Reverse2("Hello World"));
+// Console.WriteLine(Reverse2(""));
+// Console.WriteLine(Reverse2(null));
+
+// ------------- Reverse each word
+static string ReverseEachWord(string sentence) {
+        string[] words = sentence.Split(" ");
+        string[] reversedWords = words.Select(Reverse).ToArray();
+        string reversedSentance = string.Join(" ", reversedWords);
+        return reversedSentance;
+    }
+Console.WriteLine(ReverseEachWord("hello world"));
